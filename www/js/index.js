9279cb1c2212e072a763ba288211d12d604d2b36
Harold Thetiot <hthetiot@gmail.com>
2017-11-28 11:47:21 -0800

    update offline app wrapper

--- www/js/index.js
+++ www/js/index.js
@@ -1,77 +1,180 @@
-var appUrl = 'https://montagejs.github.io/popcorn/';
-
-var appLocationTimer;
-function setAppLocation(url) {
-    clearTimeout(appLocationTimer);
-    appLocationTimer = setTimeout(function() {
-
-        // Add cordova scheme if missing
-        if (url.indexOf('cordova') === -1) {
-            url += ((url.indexOf('?') > -1) ? '&' : '?') + 'cordova';
-        }
-        
-        window.location = url;
-
-    }, 250);
-}
-
-function handleOpenURL(url) {
-
-    var indentUrl,
-        scheme = 'montage',
-        schemeProtocol = scheme + ':/';
-
-    // TODO check url host
-    if (url && url.indexOf(schemeProtocol) === 0) {
-        indentUrl = url.replace(schemeProtocol, '');
-        setAppLocation(indentUrl);
-    }
-}
-
-function checkConnection() {
-
-    var networkState = window.navigator.connection.type,
-        offlineEl = document.getElementById("offline"),
-        loaderEl = document.getElementById("loader");
-
-    if (networkState === Connection.NONE) {
-        offlineEl.style.display = "block";
-        loaderEl.style.display = "none";
-    } else {
-        offlineEl.style.display = "none";
-        loaderEl.style.display = "block";
-    }
-}
-
-if (window.cordova) {
-    window.handleOpenURL = handleOpenURL;
-} else {
-    console.warn('Unable to load Cordova.')
-}
-
 var app = {
+
+    appUrl: "https://montagejs.github.io/popcorn/",
+    appCustomScheme: "montage",
+
+    appLocationArg: 'cordova',
+    appLocationDelay: 250,
+    appLocationTimer: null,
+    appLocation: null,
+
+    retryBtn: null,
+    loaderEl: null,
+    offlineEl: null,
 
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        var self = this;
+
+        self.loaderEl = document.getElementById("loader");
+        self.offlineEl = document.getElementById("offline");
+        self.retryBtn = document.getElementById("retry");
+        
+        if (self.retryBtn) {
+            self.retryBtn.addEventListener("click", this.onDeviceReady.bind(this), false);   
+        }
+
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        //document.addEventListener("online",  this.onNetworkReady.bind(this), false);
     },
 
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.getElementById("retry").addEventListener("click", this.onDeviceReady, false);
-        document.addEventListener("online",  this.onDeviceReady, false);
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    getConnection: function (resolve, reject) {
+        try {
+            
+            // Try offline using "?offline"
+            if (
+                location && 
+                    location.search && 
+                        location.search.indexOf('offline') !== -1
+            ) {
+                reject(new Error('offLine'));
+                return;
+            }   
+
+            if (
+                navigator && 
+                    navigator.connection && 
+                        navigator.connection.type &&
+                            navigator.connection.type !== Connection.UNKNOWN
+            ) {
+                if (navigator.connection.type !== Connection.NONE) {
+                    resolve(navigator.connection.type);
+                } else {
+                    reject(new Error(navigator.connection.type));
+                }
+            } else if (navigator.onLine) {
+                resolve('onLine');
+            } else {
+                reject(new Error('offLine'));
+            }
+
+        } catch (err) {
+            reject(err);
+        }
     },
-    // deviceready Event Handler
+
+    setAppLocation: function (url, resolve, reject) {
+        var self = this;
+        try {
+            // Check Url
+            if (!url) {
+                reject(new Error("Invalid Url"));
+            }
+
+            // Add customArg cordova if missing
+            var appLocationArg = self.appLocationArg;
+            if (url.indexOf(appLocationArg) === -1) {
+                url += ((url.indexOf('?') > -1) ? '&' : '?') + appLocationArg;
+            }
+
+            // Perform  delayed redirect
+            var appLocation = self.appLocation;
+            if (appLocation !== url) {    
+
+                self.appLocation = url;
+                
+                if (self.appLocationTimer) {
+                    clearTimeout(self.appLocationTimer);   
+                }
+
+                self.appLocationTimer = setTimeout(function() {
+
+                    // Clear timer
+                    self.appLocationTimer = null;
+
+                    // Resolve promise
+                    resolve(url);
+
+                    // use replace to remove history
+                    window.location.replace(url);
+
+                }, self.appLocationDelay);   
+            } else {
+
+                // Resolve
+                resolve(url);
+
+                // TODO? OR crash ?
+                //reject(new Error('Already opening same url: ' + url));
+            }
+        } catch (err) {
+            reject(err);
+        }
+    },
+
     //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {        
-        checkConnection();
-        setAppLocation(appUrl); 
+    // Events
+    // 
+
+    handleOpenURL: function (url) {
+
+        var self = this,
+            customProtocol = self.appCustomScheme && self.appCustomScheme + ':/';
+
+        if (
+            customProtocol && url &&
+                url.indexOf(customProtocol) === 0
+        ) {
+            // TODO use substr ?
+            var indentUrl = url.replace(customProtocol, '');
+            self.setAppLocation(indentUrl, function (url) {
+                console.log('handleOpenURL, will open url: ' + url);
+            }, function (err) {
+                console.log('handleOpenURL, failed cause: ' + err);
+            });  
+        }
+    },
+
+    onNetworkReady: function() {
+        var self = this,
+            appUrl = this.appLocation || this.appUrl;
+
+        self.getConnection(function (networkState) {
+            self.setAppLocation(appUrl, function (url) {
+                console.log('onNetworkReady, will open url: ' + url);
+            }, function (err) {
+                console.log('onNetworkReady, failed cause: ' + err);
+            }); 
+        });
+    },
+
+    // deviceready Event Handler
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        var self = this,
+            appUrl = this.appLocation || this.appUrl,
+            loaderEl = self.loaderEl,
+            offlineEl = self.offlineEl;
+
+        offlineEl.classList.add('hidden');
+        loaderEl.classList.remove('hidden');
+        loaderEl.classList.add('show');
+
+        self.getConnection(function (networkState) {
+            self.setAppLocation(appUrl, function (url) {
+                console.log('onDeviceReady, will open url: ' + url);
+            }, function (err) {
+                console.log('onDeviceReady, failed cause: ' + err);
+            }); 
+        }, function (err) {
+            loaderEl.classList.add('hidden');
+            offlineEl.classList.remove('hidden');
+            offlineEl.classList.add('show');
+            console.log('getConnection, failed cause: ' + err);
+        });
     }
 };
 
